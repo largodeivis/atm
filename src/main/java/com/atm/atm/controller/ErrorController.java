8d0806173853b04f02a89d24b7755179bd498094
Deivis Largo <dlargo67@gmail.com>
2022-01-10 17:25:00 -0600

    added MethodArgumentTypeMismatchException to ErrorController

--- src/main/java/com/atm/atm/controller/ErrorController.java
+++ src/main/java/com/atm/atm/controller/ErrorController.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -13,5 +14,10 @@ public class ErrorController {
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public ResponseEntity<String> handleException(HttpMessageNotReadableException exception, HttpServletRequest request) {
         return new ResponseEntity<>("Incorrect values provided to request.", HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<String> handleException(MethodArgumentTypeMismatchException exception, HttpServletRequest request) {
+        return new ResponseEntity<>("Incorrect values provided to parameters.", HttpStatus.BAD_REQUEST);
     }
 }
